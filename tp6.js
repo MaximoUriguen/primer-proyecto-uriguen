c861cb90c53001560060db8ef46d51eb2384f0ee
alumno <alumno@Instituto.local>
2023-08-04 08:56:19 -0300

    aprendiendo
    si

---
+++ tp6.js
@@ -0,0 +1,13 @@
+console.log  ( 8 - 9 ) 
+console.log  ( 8 + 9 )
+console.log ( 8 / 9 )
+console.log ( 8 * 9 )
+let numeroOcho = 8
+let numeroNueve = 9
+alert (numeroOcho)
+alert (numeroNueve)
+console.log (numeroNueve)
+console.log (numeroOcho)
+alert ('mi edad es'  ( 2023 - 2009 ) ) 
+let variableVeintitres = 2023
+let variableCeronueve = 2009
