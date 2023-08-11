0269227a3b02130a80ba73e292bfced47b6a40c0
alumno <alumno@Instituto.local>
2023-08-11 08:22:27 -0300

    si señol terminamos
    yeeep

--- tp6.js
+++ tp6.js
@@ -13,5 +13,14 @@ alert ('mi edad es ' + edad )
  let nombre = prompt ( 'cual es tu nombre ' )
  alert ('hola bienvenid@ ' + nombre)
  let edadVisitante =  prompt 
+ let nombreAlumno
+ let apellidoAlumno
 
-
+nombreAlumno =  prompt ('cual es tu Nombre')
+apellidoAlumno  =  prompt ('y tu apellido')
+alert ('bienvenid@ ' + nombreAlumno +' ' + apellidoAlumno)
+console.log ('hola ' + nombreAlumno + apellidoAlumno)
+ let diez = (10)
+ let tres = (3)
+ resultadoMultiplicacion = ( diez * tres)
+alert ('el resultado de la multiplicacion es ' + resultadoMultiplicacion)
